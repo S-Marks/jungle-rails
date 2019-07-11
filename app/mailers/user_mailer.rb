b55916be0b02e5740802391deb98778a87bdb77b
S-Marks <marks.cherie.art@gmail.com>
2019-07-11 21:55:10 +0000

    Email Confirmation of order implemented

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ApplicationMailer
+    default from: 'no-reply@jungle.com'
+
+    def order_receipt(params)
+        @user = params[:user]
+        @order = params[:order]
+        mail(to: @user.email, subject: 'Order Confirmation: Order #' + @order.id.to_s)
+    end
+end
